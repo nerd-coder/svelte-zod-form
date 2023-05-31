256a0a7c8c0b62ad0ce87e15a26bcbe94a412785
Toan <me@toan.io>
2023-05-31 14:56:10 +0700

    add test coverage

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      reporter: ['text', 'html', 'clover', 'json-summary', 'json'],
+      lines: 60,
+      branches: 60,
+      functions: 60,
+      statements: 60,
+    },
+  },
+})
