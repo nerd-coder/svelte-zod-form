3d8839dfda2b602ca2cc4c4637b64b6a0abda2a7
Toan <me@toan.io>
2023-08-08 17:20:29 +0700

    update rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,13 @@ module.exports = {
     es2017: true,
     node: true,
   },
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_', ignoreRestSiblings: true },
+    ],
+    'no-undef': 'off', // Typescript already check this
+  },
   overrides: [
     {
       files: ['*.svelte'],
